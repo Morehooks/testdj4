6b83aa03683b208dba80a07254718d6b24939b4f
jake <jakelwatson86@gmail.com>
2017-01-09 18:43:06 +0000

    add search view

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def search_form(request):
     return render(request, 'books\search_form.html')
+
+
+def search(request):
+    if 'q' in request:
+        message = 'You search for: %r ' % request.GET['q']
+    else:
+        message = 'You submitted a empty form.'
+    return HttpResponse(message)
