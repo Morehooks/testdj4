e73b6f5174f6fdcb54730b890ad339a309ece2c8
jake <jakelwatson86@gmail.com>
2017-01-09 18:37:45 +0000

    added views, template and urls for search form

--- mysite/books/admin.py
+++ mysite/books/admin.py
@@ -9,6 +9,13 @@ class AuthorAdmin(admin.ModelAdmin):
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'publisher', 'publication_date')
     list_filter = ('publication_date',)
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)
+    filter_horizontal = ('authors',)
+    raw_id_fields = ('publisher',)
+
+    # for selecting fields to edit
+    # fields = ('title', 'authors', 'publisher', 'publication_date')
 
 
 admin.site.register(Publisher)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from .views import hello, current_datetime, hours_ahead
+from mysite.books import views
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
-    # (\d{1,2}) allows two digits in regexx
+    # (\d{1,2}) allows two digits in regex
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+    url(r'^search-form/$', views.search_form),
+    url(r'^search/$', views.search),
 ]
